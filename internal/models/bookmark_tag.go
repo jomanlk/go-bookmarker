fd7e22392b78253f80516e463f209e4977233cce
jomanlk <jomanlk@gmail.com>
2025-04-20 22:28:06 +1000

    Added tag support to all endpoints (#2)

--- internal/models/bookmark_tag.go
+++ internal/models/bookmark_tag.go
@@ -1,11 +1,7 @@
 package models
 
-import "time"
-
 // BookmarkTag represents the bookmark_tag table in the database.
 type BookmarkTag struct {
-	BookmarkID int64     `json:"bookmark_id"`
-	TagID      int64     `json:"tag_id"`
-	CreatedAt  time.Time `json:"created_at"`
-	UpdatedAt  time.Time `json:"updated_at"`
+	ID        int64  `json:"id"`
+	Name      string `json:"name"`
 }
